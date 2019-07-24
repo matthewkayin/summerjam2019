a6f49d0a48627ba386c35ac181da667da1481637
matthewkayin <matthewkayin@gmail.com>
2019-07-24 17:21:41 -0500

    Initial commit
    Signed-off-by: matthewkayin <matthewkayin@gmail.com>

---
+++ game.py
@@ -0,0 +1,228 @@
+# Summer Game Jam 2019
+# Team Moral Support (Code: Matt Madden and Zubair Khan; Art: Trent Madden; Music and Sound: Kenon Brinkley)
+# game.py -- Main Class
+
+import pygame
+import os
+import ihandler
+
+
+class Game():
+    def __init__(self):
+        self.SCREEN_WIDTH = 640
+        self.SCREEN_HEIGHT = 480
+        self.TARGET_FPS = 60
+
+        self.BLACK = (0, 0, 0)
+        self.WHITE = (255, 255, 255)
+        self.RED = (255, 0, 0)
+        self.GREEN = (0, 255, 0)
+        self.BLUE = (0, 0, 255)
+
+        pygame.init()
+        self.screen = pygame.display.set_mode((self.SCREEN_WIDTH, self.SCREEN_HEIGHT))
+        self.clock = pygame.time.Clock()
+
+        # make image objects
+        # self.image_ball = pygame.image.load("res/ball.png")
+
+        # make sound objects
+        # pygame.mixer.music.load("res/storms.wav")
+        # self.beep = pygame.mixer.Sound("res/beep.wav")
+
+        pygame.font.init()
+        self.smallfont = pygame.font.SysFont("Serif", 14)
+        self.bigfont = pygame.font.SysFont("Serif", 22)
+        self.fps_text = self.smallfont.render("FPS", False, self.GREEN)
+
+        pygame.joystick.init()
+        self.AXIS_THRESHOLD = 0.001
+        self.joystick_labels = []
+        self.joystick_label_pool = "ABCDEFGHIJ"  # we shouldn't ever need more than this
+        self.joystick_count = pygame.joystick.get_count()
+        self.joystick_text = self.smallfont.render("Joysticks: " + str(self.joystick_count), False, self.GREEN)
+        for i in range(0, self.joystick_count):
+            pygame.joystick.Joystick(i).init()
+            self.joystick_labels.append(self.joystick_label_pool[i])
+        self.ihandler = ihandler.IHandler(["AXIS SNEK HORIZ", "AXIS SNEK VERT", "RESET GAME"])
+
+        self.game_init()
+
+        self.running = True
+        self.show_fps = False
+
+        self.run()
+        self.quit()
+
+    def game_init(self):
+        # pygame.mixer.music.play(-1)  # the -1 makes it play forever
+        print("put game init stuff here")
+
+    def input(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+                self.running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_F1:
+                    self.ihandler.loadMapping(False)
+                elif event.key == pygame.K_F2:
+                    self.ihandler.start_mapping()
+                elif event.key == pygame.K_F3:
+                    self.show_fps = not self.showFps
+                else:
+                    self.ihandler.key_down("K" + str(event.key))
+            elif event.type == pygame.KEYUP:
+                if event.key != pygame.K_ESCAPE and event.key != pygame.K_F1 and event.key != pygame.K_F2 and event.key != pygame.K_F3:
+                    self.ihandler.key_up("K" + str(event.key))
+            elif event.type == pygame.JOYBUTTONDOWN:
+                self.ihandler.key_down(self.joystick_labels[event.joy] + str(event.button))
+            elif event.type == pygame.JOYBUTTONUP:
+                self.ihandler.key_up(self.joystick_labels[event.joy] + str(event.button))
+            elif event.type == pygame.JOYAXISMOTION:
+                axis = self.joystick_labels[event.joy] + "x" + str(event.axis)
+                pos = pygame.joystick.Joystick(event.joy).get_axis(event.axis)
+                if self.ihandler.is_mapped_axis(axis):
+                    self.ihandler.axis_moved(axis, pos)
+                axis_pos = axis + "+"
+                axis_neg = axis + "-"
+                if abs(pos) <= self.AXIS_THRESHOLD:
+                    self.ihandler.key_up(axis_pos)
+                    self.ihandler.key_up(axis_neg)
+                elif pos > 0:
+                    self.ihandler.key_down(axis_pos)
+                    self.ihandler.key_up(axis_neg)
+                elif pos < 0:
+                    self.ihandler.key_down(axis_neg)
+                    self.ihandler.key_up(axis_pos)
+            elif event.type == pygame.JOYHATMOTION:
+                axis = self.joystick_labels[event.joy] + "t" + str(event.hat)
+                axis_h_pos = axis + "h+"
+                axis_h_neg = axis + "h-"
+                axis_v_pos = axis + "v+"
+                axis_v_neg = axis + "v-"
+                pos = pygame.joystick.Joystick(event.joy).get_hat(event.hat)
+                if pos[0] == 0:
+                    self.ihandler.key_up(axis_h_pos)
+                    self.ihandler.key_up(axis_h_neg)
+                elif pos[0] == 1:
+                    self.ihandler.key_down(axis_h_pos)
+                    self.ihandler.key_up(axis_h_neg)
+                elif pos[0] == -1:
+                    self.ihandler.key_up(axis_h_pos)
+                    self.ihandler.key_down(axis_h_neg)
+                if pos[1] == 0:
+                    self.ihandler.key_up(axis_v_pos)
+                    self.ihandler.key_up(axis_v_neg)
+                elif pos[1] == 1:
+                    self.ihandler.key_down(axis_v_pos)
+                    self.ihandler.key_up(axis_v_neg)
+                elif pos[1] == -1:
+                    self.ihandler.key_up(axis_v_pos)
+                    self.ihandler.key_down(axis_v_neg)
+
+    def update(self):
+        # handle inputs from ihandler
+        event = ""
+        while event != "EMPTY":
+            event = self.ihandler.key_queue()
+
+    def render(self):
+        self.screen.fill(self.BLACK)
+
+        # pygame.draw.rect(self.screen, self.RED, (pos[0], pos[1], 20, 20), False)
+        # self.screen.blit(self.image_ball, (pos[0], pos[1]))
+
+        if self.show_fps:
+            self.screen.blit(self.fps_text, (0, 0))
+            self.screen.blit(self.joystick_text, (0, 15))
+
+        pygame.display.flip()
+
+    def map_input(self):
+        self.screen.fill(self.BLACK)
+
+        header = self.bigfont.render("Mapping Key Inputs!", False, self.WHITE)
+        instructions = self.bigfont.render("Press the key you want for... " + self.ihandler.to_map(), False, self.WHITE)
+        self.screen.blit(header, (self.SCREEN_WIDTH / 8, self.SCREEN_HEIGHT / 2 - 50))
+        self.screen.blit(instructions, (self.SCREEN_WIDTH / 8, self.SCREEN_HEIGHT / 2 - 20))
+
+        pygame.display.flip()
+
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE or event.key == pygame.K_F1 or event.key == pygame.K_F2 or event.key == pygame.K_F3:
+                    print("You can't map those keys!")
+                    continue
+                self.ihandler.key_down("K" + str(event.key))
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_ESCAPE or event.key == pygame.K_F1 or event.key == pygame.K_F2 or event.key == pygame.K_F3:
+                    continue
+                self.ihandler.key_up("K" + str(event.key))
+            elif event.type == pygame.JOYBUTTONDOWN:
+                self.ihandler.key_down(self.joystick_labels[event.joy] + str(event.button))
+            elif event.type == pygame.JOYBUTTONUP:
+                self.ihandler.key_up(self.joystick_labels[event.joy] + str(event.button))
+            elif event.type == pygame.JOYAXISMOTION:
+                axis = self.joystick_labels[event.joy] + "x" + str(event.axis)
+                pos = pygame.joystick.Joystick(event.joy).get_axis(event.axis)
+                if self.ihandler.to_map().startswith("AXIS ") and pos != 0:
+                    self.ihandler.key_down(axis)
+                    self.ihandler.key_up("AXIS KEYUP")
+                    continue
+                axis_pos = axis + "+"
+                axis_neg = axis + "-"
+                if abs(pos) <= self.AXIS_THRESHOLD:
+                    self.ihandler.key_up(axis_neg)
+                elif pos > 0:
+                    self.ihandler.key_down(axis_pos)
+                elif pos < 0:
+                    self.ihandler.key_down(axis_neg)
+            elif event.type == pygame.JOYHATMOTION:
+                axis = self.joystick_labels[event.joy] + "t" + str(event.hat)
+                axis_h_pos = axis + "h+"
+                axis_h_neg = axis + "h-"
+                axis_v_pos = axis + "v+"
+                axis_v_neg = axis + "v-"
+                pos = pygame.joystick.Joystick(event.joy).get_hat(event.hat)
+                if pos[0] == 0:
+                    self.ihandler.key_up(axis_h_pos)
+                elif pos[0] == 1:
+                    self.ihandler.key_down(axis_h_pos)
+                elif pos[0] == -1:
+                    self.ihandler.key_down(axis_h_neg)
+                if pos[1] == 0:
+                    self.ihandler.key_up(axis_v_pos)
+                elif pos[1] == 1:
+                    self.ihandler.key_down(axis_v_pos)
+                elif pos[1] == -1:
+                    self.ihandler.key_down(axis_v_neg)
+
+    def run(self):
+        SECOND = 1000
+        before_time = pygame.time.get_ticks()
+        frames = 0
+        while self.running:
+            self.clock.tick(self.TARGET_FPS)
+            if self.ihandler.is_mapping():
+                self.map_input()
+            else:
+                self.input()
+                self.update()
+                self.render()
+                frames += 1
+
+            after_time = pygame.time.get_ticks()
+            if after_time - before_time >= SECOND:
+                # print("FPS = " + str(frames))
+                self.fps_text = self.smallfont.render('FPS: ' + str(frames), False, self.GREEN)
+                frames = 0
+                before_time += SECOND
+
+    def quit(self):
+        pygame.joystick.quit()
+        pygame.font.quit()
+        pygame.quit()
+
+
+os.environ['SDL_VIDEO_CENTERED'] = '1'  # centers the pygame window
+game = Game()
---
+++ ihandler.py
@@ -0,0 +1,160 @@
+# Summer Game Jam 2019
+# Team Moral Support (Code: Matt Madden and Zubair Khan; Art: Trent Madden; Music and Sound: Kenon Brinkley)
+# ihandler.py -- Handles input in cooperation with game class
+
+import os.path
+
+
+class IHandler():
+    def __init__(self, key_names):
+        self.names = key_names
+        self.map = []
+        self.used_inputs = []
+        self.states = [False] * len(self.names)
+        for i in range(0, len(key_names)):
+            if key_names[i].startswith("AXIS "):
+                self.states[i] = 0
+        self.queue = []
+        self.debug = False
+        self.map_index = -1
+
+        if os.path.isfile("keyconfig.txt"):
+            self.load_mapping(True)
+        elif os.path.isfile("keydefault.txt"):
+            self.load_mapping(False)
+        else:
+            print("IHandler Error - No key configs found! Key mapping is empty")
+
+    def key_down(self, key):
+        if self.map_index == len(self.map):
+            # if we've used the key before or if the key isn't an axis but we're mapping an axis, don't map the key
+            if (key in self.used_inputs
+                    or (self.names[self.map_index].startswith("AXIS ") and ("x" not in key or "+" in key or "-" in key))
+                    or (("+" in key or "-" in key) and key[:-1] in self.used_inputs)):
+                return
+            self.map.append(key)
+            # if input is axis-as-button, save only the axis
+            if "x" in key and ("+" in key or "-" in key):
+                self.used_inputs.append(key[:-1])
+            else:
+                self.used_inputs.append(key)
+            return
+        index = -1
+        for i in range(0, len(self.map)):
+            if self.map[i] == key:
+                index = i
+                break
+        if index == -1:
+            if self.debug:
+                print("IHandler Error - Requested handle keydown but key hasn't been mapped")
+            return
+        if not self.states[index]:
+            self.queue.append(index)
+            self.states[index] = True
+
+    def key_up(self, key):
+        if self.map_index != -1:
+            if self.map_index != len(self.map):
+                self.map_index += 1
+                if self.map_index == len(self.names):
+                    self.map_index = -1
+                    self.save_mapping()
+            return
+        index = -1
+        for i in range(0, len(self.map)):
+            if self.map[i] == key:
+                index = i
+                break
+        if index == -1:
+            if self.debug:
+                print("IHandler Error - Requested handle keyup but key hasn't been mapped")
+            return
+        if self.states[index]:
+            self.queue.append(-1 * (index + 1))  # all release indexes are negative, but we have to shift them all up one because there is no negative zero
+            self.states[index] = False
+
+    def axis_moved(self, key, pos):
+        index = -1
+        for i in range(0, len(self.map)):
+            if self.map[i] == key:
+                index = i
+                break
+        if index == -1:
+            print("IHandler Error - Plugged axis request even though there was no such mapped axis (this shouldn't be possible)")
+            return
+        self.states[index] = pos
+
+    def key_queue(self):
+        if len(self.queue) == 0:
+            return "EMPTY"
+        index = self.queue.pop()
+        release = ""
+        if index < 0:
+            release = " RELEASE"
+            index *= -1
+            index -= 1
+        return self.names[index] + release
+
+    def get_state(self, name):
+        index = -1
+        for i in range(0, len(self.names)):
+            if self.names[i] == name:
+                index = i
+                break
+        if index == -1:
+            print("IHandler Error - Requested check key state on a key that isn't in the keyNames. You probably made a typo.")
+            return False
+        return self.states[index]
+
+    def is_mapped_axis(self, key):
+        index = -1
+        for i in range(0, len(self.map)):
+            if self.map[i] == key:
+                index = i
+                break
+        if index == -1:
+            return False
+        return not type(self.states[index]) is bool
+
+    def is_mapping(self):
+        return self.map_index != -1
+
+    def start_mapping(self):
+        if self.map_index != -1:
+            print("IHandler Error - Can't begin input mapping, input mapping is already in progress.")
+            return
+        self.map = []
+        self.used_inputs = []
+        self.states = [False] * len(self.names)
+        for i in range(0, len(self.names)):
+            if self.names[i].startswith("AXIS "):
+                self.states[i] = 0
+        self.map_index = 0
+
+    def to_map(self):
+        if self.map_index == -1:
+            print("IHandler Error - Can't call to_map when we're not currently keymapping.")
+            return "ERROR"
+        return self.names[self.map_index]
+
+    def save_mapping(self):
+        map_file = open("keyconfig.txt", "w")
+        for i in range(0, len(self.names)):
+            map_file.write(self.names[i] + "=" + str(self.map[i]) + "\n")
+        map_file.close()
+
+    def load_mapping(self, custom):
+        self.names = []
+        fileName = "keydefaults.txt"
+        if custom:
+            fileName = "keyconfig.txt"
+        map_file = open(fileName, "r")
+        for line in map_file.read().splitlines():
+            index = line.index("=")
+            self.names.append(line[:index])
+            self.map.append(line[(index + 1):])
+        map_file.close()
+        self.states = [False] * len(self.names)
+        for i in range(0, len(self.names)):
+            if self.names[i].startswith("AXIS "):
+                self.states[i] = 0
