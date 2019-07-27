44e59aa9686d1188da557f4d94a2ef8fce256519
Zubair <zubairkhan60@gmail.com>
2019-07-27 13:59:07 -0500

    file read working - need to reacreate maps

--- room.py
+++ room.py
@@ -1,9 +1,8 @@
 class Room:
-    def __init__(self, data, x_cord, y_cord):
+    def __init__(self, file_name, x_cord, y_cord):
         self.x_cord = x_cord
         self.y_cord = y_cord
         self.tiles = []
-        for x in range(0, len(data)):
-            self.tiles.append([])
-            for y in range(0, len(data[0])):
-                self.tiles[x].append(data[x][y])
+        map_file = open("Rooms/" + file_name + ".txt", "r")
+        for line in map_file.read().splitlines():
+            self.tiles.append(list(map(int, line.split(" "))))
